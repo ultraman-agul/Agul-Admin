0b3005a0a8f177b2e984f64c86da277b15fdbd73
ultraman-agul <agulagul@163.com>
2024-05-08 17:59:40 +0800

    feat(style): 🚀 调整看房相机位置

--- src/views/three/panorama/normal/index.vue
+++ src/views/three/panorama/normal/index.vue
@@ -65,7 +65,7 @@ const initScene = () => {
   // 初始化相机
   const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height, 0.1, 1000);
   camera.position.set(0, 0, 30);
-  // camera.position.z = data.cameraZAxis;
+  camera.position.z = data.cameraZAxis;
   scene.add(camera);
   data.camera = camera;
 
